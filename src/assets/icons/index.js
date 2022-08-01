224f25ce19611e8d73063694dbf02be6e5566495
Hafif <hafifimammuddyn@gmail.com>
2022-08-01 07:41:00 +0700

    ✨ config icons

---
+++ src/assets/icons/index.js
@@ -0,0 +1,15 @@
+import AccountActive from './AccountActive.svg';
+import AccountInactive from './AccountInactive.svg';
+import HomeActive from './HomeActive.svg';
+import HomeInactive from './HomeInactive.svg';
+import OrderActive from './OrderActive.svg';
+import OrderInactive from './OrderInactive.svg';
+
+export {
+  AccountActive,
+  AccountInactive,
+  HomeActive,
+  HomeInactive,
+  OrderActive,
+  OrderInactive,
+};
