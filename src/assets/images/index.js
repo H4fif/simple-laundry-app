29041fc67d4a96ed05d552f0747c1330499191d2
Hafif <hafifimammuddyn@gmail.com>
2022-08-01 09:51:46 +0700

    ♻️ Register new images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,4 +1,8 @@
+import Header from './Header.png';
+import HeaderLogoSVG from './HeaderLogo.svg';
+import HeaderLogo from './HeaderLogo.png';
+import HeaderSVG from './Header.svg';
 import Logo from './Logo.png';
 import SplashBackground from './SplashBackground.png';
 
-export { Logo, SplashBackground };
+export { Header, HeaderLogo, HeaderLogoSVG, HeaderSVG, Logo, SplashBackground };
