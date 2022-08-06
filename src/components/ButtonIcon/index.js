110dc4193eecb58baca2240a0c89437c2930e8c7
Hafif <hafifimammuddyn@gmail.com>
2022-08-06 09:47:00 +0700

    ✨ slicing service section

--- src/components/ButtonIcon/index.js
+++ src/components/ButtonIcon/index.js
@@ -1,23 +1,48 @@
 import { Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
 import styles from './styles';
-import { AddSaldoIcon, GetPointIcon } from '../../assets';
+import {
+  AddSaldoIcon,
+  ExpressIcon,
+  GetPointIcon,
+  KarpetIcon,
+  KiloanIcon,
+  SatuanIcon,
+  SetrikaIcon,
+  VIPIcon,
+} from '../../assets';
 
-const ButtonIcon = ({ title }) => {
+const ButtonIcon = ({ title, type }) => {
   const Icon = () => {
-    if (title?.toLowerCase() === 'add saldo') return <AddSaldoIcon />;
-    if (title?.toLowerCase() === 'get point') return <GetPointIcon />;
-
-    return <AddSaldoIcon />;
+    switch (title?.trim()?.toLowerCase()) {
+      case 'add saldo':
+        return <AddSaldoIcon />;
+      case 'get point':
+        return <GetPointIcon />;
+      case 'kiloan':
+        return <KiloanIcon />;
+      case 'satuan':
+        return <SatuanIcon />;
+      case 'vip':
+        return <VIPIcon />;
+      case 'karpet':
+        return <KarpetIcon />;
+      case 'setrika saja':
+        return <SetrikaIcon />;
+      case 'express':
+        return <ExpressIcon />;
+      default:
+        return <AddSaldoIcon />;
+    }
   };
 
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity style={styles.container(type)}>
       <View style={styles.buttonWrapper}>
-        <View style={styles.iconWrapper}>
+        <View style={styles.iconWrapper(type)}>
           <Icon />
         </View>
-        <Text style={styles.buttonLabel}>{title}</Text>
+        <Text style={styles.buttonLabel(type)}>{title}</Text>
       </View>
     </TouchableOpacity>
   );
